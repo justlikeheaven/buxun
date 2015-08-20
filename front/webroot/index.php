3be13c95b6c7c021ce3327567254eab7f9805cd8
justlikeheaven <328877098@qq.com>
2015-08-21 02:12:34 +0800

    appurl

--- front/webroot/index.php
+++ front/webroot/index.php
@@ -9,14 +9,15 @@ header("charset=utf-8");
 date_default_timezone_set('Asia/Shanghai');
 
 if(false===strpos($_SERVER['SERVER_NAME'], 'local') && false===strpos($_SERVER['SERVER_NAME'], 'dev')){
-	define('ENV_PRO', 1);
+	define('ENV', 'pro');
 	error_reporting(0);
 	ini_set('display_errors', 'Off');
 }else{
-	define('ENV_PRO', 0);
+	define('ENV', 'dev');
+	define('APP_URL', 'http://localhost/buxun/front/webroot/index.php');
 	error_reporting(E_ALL ^ E_NOTICE);
 }
-define('APP_URL', 'http://localhost/buxun/front/webroot/index.php');
+
 define('APP_DIR', 'front');
 define('APP_ROOT', dirname(dirname(__DIR__)).'/');
 require '../../nfs/framework/NFS.php';
