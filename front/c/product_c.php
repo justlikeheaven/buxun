5480611e2d7d09159c68f3024cff59c649d387b0
justlikeheaven <328877098@qq.com>
2015-08-26 23:47:58 +0800

    merge

--- front/c/product_c.php
+++ front/c/product_c.php
@@ -2,22 +2,19 @@
 class product_c extends controller{
 	public function index(){
 		$id = $this->req('id', 0, 'intval');
-		if(!$id) $this->redirect('index');
+		if(!$id)	$this->redirect('index');
 		
-		$res = oo::m()->where(array('id'=>$id))->get();
+		$product = oo::m()->where(array('id'=>$id))->get();
+		if(empty($product))	$this->redirect('index');
 		$product_detail = oo::m('product_detail')->where(array('product_id'=>$id))->get();
 		if(is_array($product_detail)){
-			$res = array_merge($res, $product_detail);
+			$product = array_merge($product, $product_detail);
 		}
-		
-		$seller_detail = oo::m('seller')->where(array('id'=>$res['sellerid']))->get();
-		if(is_array($product_detail)){
-			$res = array_merge($res, $seller_detail);
-		}
-		
+		$res['product'] = $product;
+		$res['seller'] = oo::m('seller')->where(array('id'=>$product['sellerid']))->get();
 		$this->assign('res', $res);
 		
-		$seller_products = oo::m()->where(array('sellerid'=>$res['sellerid']))->orderby('id desc')->limit(5)->getall();
+		$seller_products = oo::m()->where(array('sellerid'=>$product['sellerid']))->orderby('id desc')->limit(5)->getall();
 		$this->assign('seller_products', $seller_products);
 		
 		$this->display('product');
