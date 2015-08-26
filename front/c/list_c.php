14df5dc8e102c8833b5b0ea20fdf9fb7b2043364
justlikeheaven <328877098@qq.com>
2015-08-27 00:40:42 +0800

    list

--- front/c/list_c.php
+++ front/c/list_c.php
@@ -1,11 +1,14 @@
 <?php
 class list_c extends controller{
 	public function index(){
-		$cid = $this->req('cid', 1, 'intval');
-		$total = oo::m('product')->where(array('cateid'=>$cid))->count();
+		$cid = $this->req('cid', 0, 'intval');
+		$where = array();
+		$cid && $where = array('cateid'=>$cid);
+
+		$total = oo::m('product')->where($where)->count();
 		$page = $this->req('page');
 		pager::init($page, $total);
-		$list = oo::m('product')->limit(pager::$start, pager::$num)->orderby('id desc')->where(array('cateid'=>$cid))->getall();
+		$list = oo::m('product')->limit(pager::$start, pager::$num)->orderby('id desc')->where($where)->getall();
 		$this->assign('list', $list);
 		
 		//推荐商家
