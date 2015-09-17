7fc246b75ef96c1152eda812d43dc5541222e823
justlikeheaven <328877098@qq.com>
2015-09-17 23:58:41 +0800

    list

--- front/c/list_c.php
+++ front/c/list_c.php
@@ -8,7 +8,7 @@ class list_c extends controller{
 		$total = oo::m('product')->where($where)->count();
 		$page = $this->req('page');
 		pager::settpl(oo::cfg('pager'));
-		pager::init($page, $total, 12);
+		pager::init($page, $total, 18);
 		$this->assign('pager', pager::get());
 		$list = oo::m('product')->limit(pager::$start, pager::$num)->orderby('id desc')->where($where)->getall();
 		$this->assign('list', $list);
