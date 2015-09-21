a532645930638c7c39483a119c2fa98ee19ed909
justlikeheaven <328877098@qq.com>
2015-09-21 23:43:43 +0800

    index

--- docs/u.sql
+++ docs/u.sql
@@ -1,5 +1,9 @@
-ALTER TABLE  `product_cate` CHANGE  `img`  `img` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '' COMMENT  '图片';
-
 ALTER TABLE  `seller` ADD  `banner` VARCHAR( 50 ) NOT NULL DEFAULT  '' COMMENT  'banner' AFTER  `img`;
 ALTER TABLE  `seller` ADD  `photo` VARCHAR( 500 ) NOT NULL DEFAULT  '' COMMENT  '商家介绍图' AFTER  `banner`;
-
+CREATE TABLE `seller_hot` (
+ `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+ `seller_id` int(10) unsigned NOT NULL COMMENT '商家id',
+ `hot` tinyint(1) unsigned NOT NULL COMMENT '是否最热，0-否，1-是',
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `product_id` (`seller_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='最热商家'
--- front/c/index_c.php
+++ front/c/index_c.php
@@ -20,15 +20,14 @@ class index_c extends controller{
 		$this->assign('product_hot', $product_hot);
 		
 		$except_hot = '';
-		if(is_array($hot) && !empty($hot)){
-			foreach ($hot as $v){
+		if(is_array($product_hot) && !empty($product_hot)){
+			foreach ($product_hot as $v){
 				$hotid[] = $v['id'];
 			}
 			$except_hot = ' and id not in ('.implode(', ', $hotid).')';
 		}
 		//获取分类下的最新
 		$cate = oo::m('product_cate')->getall();
-
 		foreach ($cate as $v){
 			$value = array('img'=>$v['img'], 'id'=>intval($v['id']));
 			if($v['name']=='针织'){
@@ -38,9 +37,11 @@ class index_c extends controller{
 			}
 			$catearr[$catename] = array('img'=>$v['img'], 'id'=>$v['id']);
 		}
+		//针织
 		$list['zz']['img'] = $catearr['zz']['img'];
 		$catearr['zz']['id'] && $list['zz']['list'] = oo::m('product')->where("cateid={$catearr['zz']['id']} {$except_hot}")->limit(8)->orderby('id desc')->getall();//最新针织
-		
+
+		//梭织
 		$list['sz']['img'] = $catearr['sz']['img'];
 		$catearr['sz']['id'] && $list['sz']['list'] = oo::m('product')->where("cateid={$catearr['sz']['id']} {$except_hot}")->limit(8)->orderby('id desc')->getall();//最新梭织
 		$this->assign('list', $list);
